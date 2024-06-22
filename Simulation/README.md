44a76607bb1e8e98969ce51fe6f528cd8cfd9076
Anantha Padmanabhan M Nair <apm.nair@niser.ac.in>
2024-06-22 12:47:25 +0530

    Added simulation files

---
+++ Simulation/README.md
@@ -0,0 +1,12 @@
+<h1> Running the Simulation</h1>
+
+<ul>
+    <li> Forst enter the environment by executing:
+    <pre><code>alienv enter VO_ALICE@FOCAL::latest VO_ALICE@AliDPG::latest</code></pre></li>
+    <li> To run the simulation run the following command in the terminal:
+        <pre><code>./runSimulation.sh n</code></pre>
+        Where n is the required number of times the simulations has to run</li>
+    <li> If you want to change the Particle, geometry or generator, you can edit the <code>runSimulation.sh</code></li>
+    <li>The Output files will be stored in the folder: <code>MSc_Projct/Simulation_outputs</code> </li>
+</ul>
+    
---
+++ Simulation/runSimulation.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+
+####################
+#    Parameters    #
+####################
+run="294925"
+mode="sim"
+detector="FOCAL"
+uid="1"
+simulation="NoDigitization"
+geometry_location="GeometryFiles/geometry.txt"
+nevents="199"
+generator="Upgrade:FOCAL_Generators:boxMomUniform"
+pdg="111"
+etamin="3.4"
+etamax="5.3"
+ptmin="0.0"
+ptmax="1500.0"
+
+num=$1
+for ((i=1; i<=num; i++))
+do
+  echo "#############################"
+  echo "#  Running simulation: $i   #"
+  echo "#############################"
+
+
+
+
+
+  ####################
+  #    Target_DIR    #
+  ####################
+  SOURCE_DIR="."
+  TARGET_DIR="../Simulation_outputs/pdg"$pdg"_nevents$nevents/$i"
+  mkdir -p $TARGET_DIR
+
+  ##########################
+  #    Print_parameters    #
+  ##########################
+  echo "Running simulation with the following parameters:"
+  echo "Source directory      :       $SOURCE_DIR"
+  echo "Target directory      :       $TARGET_DIR"
+  echo "UID                   :       $uid"
+  echo "mode                  :       $mode"
+  echo "simulation            :       $simulation"
+  echo "Run number            :       $run"
+  echo "Mode                  :       $mode"
+  echo "Detector              :       $detector"
+  echo "Generator             :       $generator"
+  echo "PDG ID                :       $pdg"
+  echo "Number of events      :       $nevents"
+  echo "Eta range             :       $etamin - $etamax"
+  echo "Pt range              :       $ptmin - $ptmax"
+
+
+  ##########################
+  #     Run Simulation     #
+  ##########################
+  /home/apmnair18/alice/sw/ubuntu2204_x86-64/AliDPG/master-local1/MC/dpgsim.sh --run $run --mode $mode --detector $detector --uid $uid --nevents $nevents --generator $generator --simulation $simulation --focalGeometryFile $geometry_location --pdg $pdg --etamin $etamin --etamax $etamax --ptmin $ptmin --ptmax $ptmax
+
+
+  ###############################################
+  # Move the GRP folder to the target directory #
+  ###############################################
+  if [ -d "$SOURCE_DIR/GRP" ]; then
+    mv "$SOURCE_DIR/GRP" "$TARGET_DIR/"
+  fi
+
+
+  ######################################################
+  # Move all output ROOT files to the target directory #
+  ######################################################
+  for file in "$SOURCE_DIR"/*.root; do
+    if [[ "$file" != "$SOURCE_DIR/OCDBsim.root" && "$file" != "$SOURCE_DIR/OCDBrec.root" ]]; then
+      mv "$file" "$TARGET_DIR/"
+    fi
+  done
+  mv "$SOURCE_DIR/grpdump.sh" "$TARGET_DIR/"
+  mv "$SOURCE_DIR/gphysi.dat" "$TARGET_DIR/"
+  mv "$SOURCE_DIR/sim.log" "$TARGET_DIR/"
+  mv "$SOURCE_DIR/simwatch.log" "$TARGET_DIR/"
+  mv "$SOURCE_DIR/MCStepLoggerVolMap.dat" "$TARGET_DIR/"
+done
+
+
+
+
+
+
